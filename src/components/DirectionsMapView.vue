c61260ac0e4b4ecdc8f0482e627b66490618eea4
xrchitron <yuahochen1124@gmail.com>
2023-11-04 14:20:58 +0800

    feat: map function

--- src/components/DirectionsMapView.vue
+++ src/components/DirectionsMapView.vue
@@ -1,3 +1,87 @@
+<script>
+import { EventBus } from '../../EventBus.js';
+
+export default {
+  mounted() {
+    const map = new google.maps.Map(this.$el, {
+      center: { lat: 25.0330, lng: 121.5654 },
+      zoom: 15,
+      mapTypeId: 'roadmap',
+    });
+    const directionsService = new google.maps.DirectionsService();
+    EventBus.on('routes-data', (routes) => {
+    const newMap = new google.maps.Map(this.$el, {
+      center: { lat: 25.0330, lng: 121.5654 },
+      zoom: 15,
+      mapTypeId: 'roadmap',
+    });
+
+      routes.forEach((data) => {
+        directionsService.route({
+          origin: `${data.Origin.lat},${data.Origin.lng}`,
+          destination: `${data.Destination.lat},${data.Destination.lng}`,
+          travelMode: "DRIVING",
+        }, (response, status) => {
+          if (status === 'OK') {
+            const directionsRenderer = new google.maps.DirectionsRenderer(
+              {
+                suppressMarkers: true,
+                directions: response,
+                map: newMap,
+                polylineOptions: {
+                  strokeColor: data.color,
+                  strokeWeight: 8,
+                }
+              }
+            );
+            this.createInfoWindowWith(data.Origin, "marker alternate icon", newMap)
+            this.createInfoWindowWith(data.Destination, "flag outline icon", newMap)
+            
+            const overviewPath = response.routes[0].overview_path;
+            const middleIndex = parseInt(overviewPath.length / 2);
+            const middleLoc = overviewPath[middleIndex];
+
+            const distanceDurationLabel = new google.maps.InfoWindow({
+              content: `<i class="icon car"></i>${data.distanceText} - ${data.durationText}`,
+              position: new google.maps.LatLng(middleLoc.lat(), middleLoc.lng())
+            });
+            distanceDurationLabel.open(newMap,  null);
+
+            this.createPolylineWith([
+                {lat: data.Origin.lat, lng: data.Origin.lng},
+                {lat: overviewPath[0].lat(), lng: overviewPath[0].lng()},
+              ], data.color, newMap)
+            this.createPolylineWith([
+                {lat: data.Destination.lat, lng: data.Destination.lng},
+                {lat: overviewPath[overviewPath.length - 1].lat(), lng: overviewPath[overviewPath.length - 1].lng()},
+              ], data.color, newMap)
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      })
+    });
+  },
+  methods: {
+    createInfoWindowWith(location, icon, map){
+      const infoWindow = new google.maps.InfoWindow({
+              content: `<i class="${icon}"></i>${location.name}`,
+              position: new google.maps.LatLng(location.lat, location.lng)
+            });
+            infoWindow.open(map,  null);
+    },
+    createPolylineWith(path, color, map){
+      new google.maps.Polyline({
+        path,
+        strokeColor: color,
+        strokeOpacity: 1.0,
+        strokeWeight: 8,
+        map
+      })
+    }
+  }
+}
+</script>
 <template>
   <section class="map"></section>
 </template>
@@ -9,5 +93,11 @@
   bottom: 0;
   left: 0;
   background-color: turquoise;
+}
+.gm-style-iw button{
+  display: none !important;
+}
+.gm-style .gm-style-iw-c{
+  padding: 5px !important;
 }
 </style>
--- src/components/RouteList.vue
+++ src/components/RouteList.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios';
+import {EventBus} from '../../EventBus.js';
 export default {
   data(){
     return {
@@ -10,7 +11,11 @@ created(){
   axios
   .post('http://localhost:3000/v1/maps/routes',{
     itineraryId: 1,
-    date: "2023-01-01"
+    date: "2023-01-01",
+    sort: {
+      key: 'distance',
+      order: 'asc'
+    }
   })
   .then((res) => {
     if (res.status === 200) {
@@ -20,6 +25,35 @@ created(){
   .catch((err) => {
     console.log(err);
   })
+},
+methods:{
+  sortRoute(e){
+    const sort = e.target.value;
+    const [key, order] = sort.split('-');
+    axios
+    .post('http://localhost:3000/v1/maps/routes',{
+    itineraryId: 1,
+    date: "2023-01-01",
+    sort: {
+      key,
+      order
+    }
+  })
+  .then((res) => {
+    if (res.status === 200) {
+      this.routes =[...res.data.data];
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  },
+  routeItemPressed(route){
+    EventBus.emit('routes-data', [route]); // emit event
+  },
+  showAllRoutesButtonPressed(){
+    EventBus.emit('routes-data', this.routes); // emit event
+  }
 }
 }
 </script>
@@ -27,12 +61,24 @@ created(){
   <section class="route-list-view">
     <div class="route-list-view-header">
       <h3 class="ui header">Route List</h3>
+      <select @change="sortRoute($event)">
+        <option selected disable>Sort</option>
+        <optgroup label="Distance">
+          <option value="distance-asc">short</option>
+          <option value="distance-desc">long</option>
+        </optgroup>
+        <optgroup label="Duration">
+          <option value="duration-asc">fast</option>
+          <option value="duration-desc">slow</option>
+        </optgroup>
+      </select>
+      <button class="ui button show-all" @click="showAllRoutesButtonPressed">Show All</button>
     </div>
-    <div class="item" v-for="route in routes" :key="route.id">
-      <div>{{ route.Origin.name }}</div>
-      <div>{{ route.Destination.name }}</div>
-      <div>{{ route.distanceText }}</div>
-      <div>{{ route.durationText }}</div>
+    <div class="item" v-for="route in routes" :key="route.id" @click="routeItemPressed(route)" :style="{'background-color':route.color}">
+      <div><i class="marker alternate icon"></i>{{ route.Origin.name }}</div>
+      <div><i class="flag checkered icon"></i>{{ route.Destination.name }}</div>
+      <div class="ui label small">{{ route.distanceText }}</div>
+      <div class="ui label small">{{ route.durationText }}</div>
     </div>
   </section>
 </template>
@@ -50,5 +96,11 @@ created(){
 .item{
   padding: 10px;
   cursor: pointer;
+}
+.item:hover{
+  background-color: rgba(0,0,0,.1);
+}
+.show-all{
+  padding: 4px 10px;
 }
 </style>
